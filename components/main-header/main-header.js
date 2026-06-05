dd20d0df05125ddcad1fcb6821f6bd65a0dae9e9
Adam Ramski <adam.ramski@gmail.com>
2026-06-05 14:36:22 +0200

    add NavLink component

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,8 +1,10 @@
-import logoImg from "@/assets/logo.png";
-import MainHeaderBackground from "@/components/main-header/main-header-background";
-import Image from "next/image";
-import Link from "next/link";
-import classes from "./main-header.module.css";
+import Link from 'next/link';
+import Image from 'next/image';
+
+import MainHeaderBackground from './main-header-background';
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
   return (
@@ -10,20 +12,17 @@ export default function MainHeader() {
       <MainHeaderBackground />
       <header className={classes.header}>
         <Link className={classes.logo} href="/">
-          <Image src={logoImg} alt="A plate with a food on it" priority />
+          <Image src={logoImg} alt="A plate with food on it" priority />
           NextLevel Food
         </Link>
 
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/meals/share">Share Meal</Link>
-            </li>
-            <li>
-              <Link href="/community">Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
