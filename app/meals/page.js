2c28b7a0ba1472a762fbe9a39ad6f720f2c48d93
Adam Ramski <adam.ramski@gmail.com>
2026-06-05 16:22:12 +0200

    add loading state to the meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 import classes from "./page.module.css";
 
-export default async function MealsPage() {
+const Meals = async () => {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+};
+
+export default async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -22,7 +27,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
