6c35966f243d9bc5fcf0f4e5bd8999b328f721e9
Adam Ramski <adam.ramski@gmail.com>
2026-06-05 17:01:13 +0200

    add meal details page + meal data fetching

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,10 +1,41 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+
 export default async function MealPage({ params }) {
   const { slug } = await params;
+  const meal = getMeal(slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br>");
 
   return (
-    <main>
-      <h1>Meal Page:</h1>
-      <p>This is the meal page for the meal with the slug: {slug}</p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
